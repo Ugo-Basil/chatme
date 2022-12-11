04edc06f86e3ec9b868c116dc3eac9084138181c
Basil <ohuruogubasil@gmail.com>
2022-12-12 00:26:48 +0100

    setup:client/server

---
+++ chatmeserver/schemas/index.js
@@ -0,0 +1,14 @@
+import user from "./user"
+import postedBy from "./postedBy"
+import comment from "./comment"
+import pin from "./pin"
+import save from "./save"
+
+
+export const schemaTypes = [
+    user,
+    postedBy,
+    comment,
+    pin,
+    save,
+]
